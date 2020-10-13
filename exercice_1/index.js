a75e5a9d8421f77bf0d5ead4ab6c1a068098a461
Farès <faresboukhezna@gmail.com>
2020-10-13 18:03:42 +0200

    exercice 1, tableau qui change de couleur

---
+++ exercice_1/index.js
@@ -0,0 +1,44 @@
+function getRandomInteger(min , max){
+
+    return Math.floor(Math.random() * (max - min) + min);
+
+}
+
+function gridGenerator(xAxis, yAxis){
+
+    var body = document.getElementById("mainBody");
+
+    //On clear le tableau précédent avant d'en créer un nouveau.
+    body.innerHTML = '';
+
+    var table = document.createElement("table");
+
+    for(var i = 0; i < xAxis; i++){
+
+        var tr = document.createElement("tr");
+
+        for(var j = 0; j < yAxis; j++){
+
+            var r = getRandomInteger(0,255);
+            var g = getRandomInteger(0,255);
+            var b = getRandomInteger(0,255);
+
+            var colorAttribute = "rgb("+r+","+g+","+b+")";
+
+            var cell = document.createElement("td");
+            cell.style.backgroundColor = colorAttribute;
+
+            tr.appendChild(cell);
+
+        }
+
+        table.appendChild(tr);
+    }
+
+    body.appendChild(table);
+
+}
+
+//on la démarre une première fois avant de lancer l'intervalle
+gridGenerator(10,10);
+setInterval(function(){gridGenerator(10,10)}, getRandomInteger(1000, 2000));
