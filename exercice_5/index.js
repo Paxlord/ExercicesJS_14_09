fd1cbfa226e588c8458ae468768b2249a57342f3
Farès <faresboukhezna@gmail.com>
2020-10-13 22:30:34 +0200

    exercice_5, formulaire, BUGGUE

---
+++ exercice_5/index.js
@@ -0,0 +1,59 @@
+var regexNameSurname = /([A-Za-z])+/g;
+var regexAddress = /^[1-9]+([ A-Za-z.']){2,}$/g;
+var regexEmail = /^[1-9a-zA-z]+@[a-z]+[.][a-z]+/g;
+var regexPassword = /([a-zA-Z1-9]){8,}/
+
+
+
+function validateForm(evt){
+
+    evt.preventDefault();
+
+    var surname = document.forms['form']['surname'].value;
+
+    if(surname == '' || !regexNameSurname.test(surname)){
+        document.getElementById("surnameError").className = "doDisplay";;
+    }else{
+        document.getElementById("nameError").className = "doNotDisplay";
+    }
+
+    var name = document.forms['form']['name'].value;
+
+    if(name == '' || !regexNameSurname.test(name)){
+        document.getElementById("nameError").className = "doDisplay";
+    }else{
+        document.getElementById("nameError").className = "doNotDisplay";
+    }
+
+    var address = document.forms['form']['address'].value;
+
+    if(address == '' || !regexAddress.test(address)){
+        document.getElementById("addressError").className = "doDisplay";
+    }else{
+        document.getElementById("addressError").className = "doNotDisplay";
+    }
+
+
+    var email = document.forms['form']['email'].value;
+
+    if(email == '' || !regexEmail.test(email)){
+        document.getElementById("emailError").className = "doDisplay";
+    }else{
+        document.getElementById("emailError").className = "doNotDisplay";
+    }
+
+
+    var password = document.forms['form']['password'].value;
+
+    if(password == '' || !regexNameSurname.test(password)){
+        document.getElementById("passwordError").className = "doDisplay";
+    }else{
+        document.getElementById("passwordError").className = "doNotDisplay";
+    }
+
+
+
+}
+
+var form = document.getElementsByName("form")[0];
+form.addEventListener("submit", validateForm);
