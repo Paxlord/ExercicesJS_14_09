b53cc90113934c4277adf29a721056f42417898e
Farès <faresboukhezna@gmail.com>
2020-10-13 23:39:29 +0200

    exercice9

---
+++ exercice_9/index.js
@@ -0,0 +1,35 @@
+function CheckPalindrome(str){
+
+    var cleanedUpString = "";
+
+    //On enlève tous les espaces
+    for(var i = 0; i < str.length; i++){
+        if(str[i] !== ' '){
+            cleanedUpString += str[i];
+        }
+    }
+
+    //On inverse la str
+    var reversedStr = "";
+    for(var i = cleanedUpString.length - 1; i >= 0; i--){
+        reversedStr += cleanedUpString[i];
+    }
+
+    console.log("Original string : ",cleanedUpString);
+    console.log("Inversed string : ", reversedStr);
+
+    //On parcours les deux strings et si jamais ça ne match pas on return false
+    for(var i = 0; i < cleanedUpString.length; i++){
+        if(cleanedUpString[i] !== reversedStr[i]){
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+console.log(CheckPalindrome("test"));
+console.log(CheckPalindrome("ete"));
+console.log(CheckPalindrome("ceci est un palindrome"));
+console.log(CheckPalindrome("my gym"));
+console.log(CheckPalindrome("no lemon no melon"));
